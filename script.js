9a8cf09eed94e9db2858a3ce35b637c695d018f6
Md.Nure Ala Moududi <noyon.moududi@gmail.com>
2024-03-05 12:45:09 +0600

    script and index page update

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Create a link element
-  const linkElement = document.createElement("link");
-  linkElement.rel = "stylesheet";
-  linkElement.href = "https://noyonmoududi.github.io/milao_chatbot/style.css";
-  //linkElement.href = "style.css";
-  // Append the link element to the head of the document
-  document.head.appendChild(linkElement);
-  // Create a new div element
-  const chatbotContainer = document.createElement("div");
-  // Set the attributes for the div
-  chatbotContainer.className = "milaoChatbotWrapper";
-  chatbotContainer.id = "chatbotContainer";
-  // Append the div to the body of the document
-  document.body.appendChild(chatbotContainer);
+// Create a new div element
+const chatbotContainer = document.createElement("div");
+// Set the attributes for the div
+chatbotContainer.className = "milaoChatbotWrapper";
+chatbotContainer.id = "chatbotContainer";
+// Append the div to the body of the document
+document.body.appendChild(chatbotContainer);
 
-  const chatIcon = document.getElementById("chatbotBtn");
-  const wrapper = document.querySelector(".milaoChatbotWrapper");
+const chatIcon = document.getElementById("chatbotBtn");
+const wrapper = document.querySelector(".milaoChatbotWrapper");
 
-  chatIcon.addEventListener("click", function () {
-    if (chatIcon.classList.contains("active")) {
-      wrapper.style.opacity = 0;
-      wrapper.style.bottom = "0";
-      wrapper.style.pointerEvents = "none";
-      chatIcon.classList.remove("active");
-    } else {
-      wrapper.style.opacity = 1;
-      wrapper.style.bottom = "85px";
-      wrapper.style.pointerEvents = "auto";
-      chatIcon.classList.add("active");
-      initChatbot();
-    }
-  });
+chatIcon.addEventListener("click", function () {
+  if (chatIcon.classList.contains("active")) {
+    wrapper.style.opacity = 0;
+    wrapper.style.bottom = "0";
+    wrapper.style.pointerEvents = "none";
+    chatIcon.classList.remove("active");
+  } else {
+    wrapper.style.opacity = 1;
+    wrapper.style.bottom = "85px";
+    wrapper.style.pointerEvents = "auto";
+    chatIcon.classList.add("active");
+    initChatbot();
+  }
 });
 
 function initChatbot() {
